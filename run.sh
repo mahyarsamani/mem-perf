ddf1ebfaa52b0594924538ca8a31b1e06420408a
Mahyar Samani <msamani@ucdavis.edu>
2021-10-21 09:38:50 -0700

    Adding run.sh and updated plots.

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+generators=(Linear Random)
+intensities=(Loaded Unloaded)
+memories=(DDR3 DDR4 LPDDR3 HBM)
+num_channels=(1)
+
+for generator in "${generators[@]}"
+    do for intensity in "${intensities[@]}"
+        do for memory in "${memories[@]}"
+            do for num_chnls in "${num_channels[@]}"
+                do
+                gem5/build/NULL_MESI_Two_Level/gem5.opt -re --outdir=results/memory_studies/$generator/$intensity/$memory/$num_chnls test_memory.py $generator $intensity $memory $num_chnls &
+            done
+        done
+    done
+done
+
+caches=(NoCache PrivateL1 PrivateL1PrivateL2 MESITwoLevel)
+
+for cache in "${caches[@]}"
+    do
+    gem5/build/NULL_MESI_Two_Level/gem5.opt -re --outdir=results/cache_studies/$cache test_cache.py $cache &
+done
